e3f1d466e0e526d20226bf35d135df6f43e564e1
Amit Kumar Gupta <amit1704gupta@gmail.com>
2025-05-29 00:28:03 +0530

    first commit

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, "/public")))
 
 
 const sessionOptions = {
-  secret: "mysuperSecretcode",
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie:{
